52c64bb90dcf0ae33f5fde827edc962f887de8e0
KorYejinLee <koryjlee@gmail.com>
2025-03-16 16:17:35 +0900

    #1 fix: 닉네임 입력폼 필드 추가

--- src/app/components/signup-form.tsx
+++ src/app/components/signup-form.tsx
@@ -12,6 +12,10 @@ export function SignupForm() {
         <input id="email" name="email" type="email" placeholder="Email" />
       </div>
       <div>
+        <label htmlFor="nickname">Nicknme</label>
+        <input id="Nickname" name="Nickname" placeholder="NickName" />
+      </div>
+      <div>
         <label htmlFor="password">Password</label>
         <input id="password" name="password" type="password" />
       </div>
